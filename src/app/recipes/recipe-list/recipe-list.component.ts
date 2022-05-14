a10280322d9b01ab50914f9c7033555258282ca1
Lucas Couto <loc.unb@gmail.com>
2022-05-13 21:59:03 -0300

    Adds recipes list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    {
+      name: 'A test recipe',
+      description: 'this is a simply test',
+      imagePath:
+        'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+    },
+    {
+      name: 'A test recipe',
+      description: 'this is a simply test',
+      imagePath:
+        'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+    },
+  ];
 
   constructor() {}
 
