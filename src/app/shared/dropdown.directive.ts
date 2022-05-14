66789044354d8439627821732c0a6b8a2ee98b5d
Lucas Couto <loc.unb@gmail.com>
2022-05-14 18:49:05 -0300

    Rename function from onOpen to toggleOpen

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -6,7 +6,7 @@ import { Directive, HostBinding, HostListener } from '@angular/core';
 export class DropdownDirective {
   @HostBinding('class.open') isOpened = false;
 
-  @HostListener('click') onClick(): void {
+  @HostListener('click') toggleOpen(): void {
     this.isOpened = !this.isOpened;
   }
 }
