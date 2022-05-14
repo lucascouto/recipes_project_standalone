593624ed637ec92d58af74d34f3264fcaacff32e
Lucas Couto <loc.unb@gmail.com>
2022-05-14 15:00:20 -0300

    Uses EventEmitter and ngIf to show menus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  loadedMenu = 'recipe';
+
+  onNavigate(menu: string): void {
+    this.loadedMenu = menu;
+  }
+}
