c2936e8722381030feae403724b5ec6e741fcf90
Lucas Couto <loc.unb@gmail.com>
2022-05-13 23:10:55 -0300

    Adds shopping list component and ingredient model

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    {
+      name: 'Apples',
+      amount: 5,
+    },
+    {
+      name: 'Tomatoes',
+      amount: 10,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
