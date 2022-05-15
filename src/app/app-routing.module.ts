5f587bcb03d90a3dd3230fb498b4c4d0d70667ce
Lucas Couto <loc.unb@gmail.com>
2022-05-15 20:48:11 -0300

    Organize imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
