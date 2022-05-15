54a214378761e6ee931a54107b5364f768cbcdfc
Lucas Couto <loc.unb@gmail.com>
2022-05-15 01:03:16 -0300

    Creates ingredients array for a recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,12 +9,20 @@ export class RecipeService {
       description: 'this is a simply test',
       imagePath:
         'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+      ingredients: [
+        { name: 'Meat', amount: 1 },
+        { name: 'French Fries', amount: 20 },
+      ],
     },
     {
       name: 'Another recipe',
       description: 'another teste recipe',
       imagePath:
         'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      ingredients: [
+        { name: 'Buns', amount: 2 },
+        { name: 'Meat', amount: 1 },
+      ],
     },
   ];
 
