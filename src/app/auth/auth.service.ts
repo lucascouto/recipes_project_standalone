48502b1af6e6f0cc06236b1aaf051e103c9bc84f
Lucas Couto <loc.unb@gmail.com>
2022-05-24 22:38:13 -0300

    Creates authService with signup method

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+interface AuthResponse {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private BASE_URL = 'https://identitytoolkit.googleapis.com/v1/accounts:';
+  private API_KEY = 'AIzaSyCFuOlGgMVDWXL4jNuaqhNywNogWTgk42E';
+
+  constructor(private http: HttpClient) {}
+
+  signup(email: string, password: string) {
+    return this.http.post<AuthResponse>(
+      `${this.BASE_URL}/signUp?key=${this.API_KEY}`,
+      {
+        email,
+        password,
+        returnSecureToken: true,
+      }
+    );
+  }
+}
