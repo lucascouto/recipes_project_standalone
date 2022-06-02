f032a8f2d418718cd158bb08c535ab0c82eb1cca
Lucas Couto <loc.unb@gmail.com>
2022-06-01 21:50:53 -0300

    Imports sharedModule

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AuthComponent } from './auth.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [AuthComponent],
-  imports: [CommonModule, FormsModule, AuthRoutingModule],
+  imports: [CommonModule, FormsModule, AuthRoutingModule, SharedModule],
 })
 export class AuthModule {}
