2b3b062fd1676c423e26bbbc67cdded8ea431e99
Lucas Couto <loc.unb@gmail.com>
2022-06-01 21:47:48 -0300

    Creates auth module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AuthComponent } from './auth/auth.component';
-
 const appRoutes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'recipes' },
-  { path: 'auth', component: AuthComponent },
 ];
 
 @NgModule({
